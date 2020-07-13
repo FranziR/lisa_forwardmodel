bdb4e2c670a4af8e968c0f5cebcacbecc64785c3
Franziska Riegger <Franziska.Riegger@erdw.ethz.ch>
2020-07-13 15:49:56 +0200

    Created setup file.

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='lisa_forwardmodel',
+    version='0.1',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='Franziska Riegger',
+    author_email='Franziska.Riegger@erdw.ethz.ch',
+    description='lisa_forwardmodel simulates the LISA response without noise.',
+    long_description=long_description,
+    classifiers=[
+            "Programming Language :: Python :: 3",
+            "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.8'
+)
