d49844f2fc0f08e222e30d2241f89eadf3f6d04c
Franziska Riegger <Franziska.Riegger@erdw.ethz.ch>
2020-07-14 18:48:16 +0200

    Load conda env from environment.yml.

--- config.sh
+++ config.sh
@@ -19,39 +19,6 @@ conda_installation()
     conda config --set always_yes yes --set changeps1 no
     conda update -q conda
     conda info -a
-
-#    if [ -d "$download_dir" ]; then # check whether directory temp (for download) exists: -d
-#       if [ "$(ls -A "$download_dir")" ]; then # Check whether existing directory is empty
-#         empty=false
-#         for file in "$download_dir"/*; # check whether installation file is in $download_dir
-#         do
-#           if [[ ${file: -3} == ".sh" ]]; then
-#             installation_file=$file
-#           fi
-#         done;
-#       else
-#         empty=true
-#       fi
-#    else
-#       mkdir "temp"
-#       empty=true
-#    fi
-
-#   if [ $empty != false ]; then # if $download_dir is empty, download file
-#     wget $link -P $download_dir -O anaconda.sh
-#   fi
-
-#   printf 'yes\nqyes\n\n' | bash ./"$download_dir/$installation_file" # install file
-#   ( echo -ne "yes\n" ; echo -ne "\n"; echo -ne "\n" ) | bash ./"$download_dir/$installation_file" # install file
-#   echo -ne '\n' | <yourfinecommandhere>
 }
-#
-#if [[ "$(conda --version)" == "conda "* ]];
-#then
-# echo "Success."
-#else
-#  echo "Call installation function."
-#  conda_installation()
-#fi
 
 conda_installation
