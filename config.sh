ee4e8576cb865721f23863cfa8973c10a96ce359
Franziska Riegger <Franziska.Riegger@erdw.ethz.ch>
2020-07-14 13:33:19 +0200

    Added config shell script to install conda.

---
+++ config.sh
@@ -0,0 +1,52 @@
+#!bin/bash
+# aim of the script is to
+# 1. check whether conda version exits
+# 2. if not, download it and install
+
+conda_installation()
+{
+    download_dir="temp" # check which OS to define download link and name of installation file
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then #  echo "Linux"
+        link="https://repo.anaconda.com/archive/Anaconda3-2020.02-Linux-ppc64le.sh"
+        installation_file="Anaconda3-2020.02-Linux-ppc64le.sh"
+     elif [[ "$OSTYPE" == "darwin"* ]]; then #   echo "Mac"
+        link="https://repo.anaconda.com/archive/Anaconda3-2020.02-MacOSX-x86_64.sh"
+        installation_file="Anaconda3-2020.02-MacOSX-x86_64.sh"
+     elif [[ "$OSTYPE" == "win32" ]]; then #   echo "Windows"
+        link="https://repo.anaconda.com/archive/Anaconda3-2020.02-Windows-x86.exe"
+        installation_file="Anaconda3-2020.02-Windows-x86.exe"
+    fi
+
+    if [ -d "$download_dir" ]; then # check whether directory temp (for download) exists: -d
+       if [ "$(ls -A "$download_dir")" ]; then # Check whether existing directory is empty
+         empty=false
+         for file in "$download_dir"/*; # check whether installation file is in $download_dir
+         do
+           if [[ ${file: -3} == ".sh" ]] || [[ ${file: -4} == ".exe" ]]; then
+             installation_file=$file
+           fi
+         done;
+       else
+         empty=true
+       fi
+    else
+       mkdir "temp"
+       empty=true
+    fi
+
+   if [ $empty != false ]; then # if $download_dir is empty, download file
+     wget $link -P $download_dir
+   fi
+
+   bash ./"$download_dir/$installation_file" # install file
+}
+#
+#if [[ "$(conda --version)" == "conda "* ]];
+#then
+# echo "Success."
+#else
+#  echo "Call installation function."
+#  conda_installation()
+#fi
+
+conda_installation
