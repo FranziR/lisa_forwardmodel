b990f277b73d120261aeb0e7c3158d3717c6f6dd
Franziska Riegger <Franziska.Riegger@erdw.ethz.ch>
2020-07-14 14:25:10 +0200

    Added config shell script to install conda.

--- config.sh
+++ config.sh
@@ -38,7 +38,7 @@ conda_installation()
      wget $link -P $download_dir
    fi
 
-   printf 'yes\n\n\n' | bash ./"$download_dir/$installation_file" # install file
+   printf 'yes\n\nq' | bash ./"$download_dir/$installation_file" # install file
 #    bash ./"$download_dir/$installation_file" # install file
 #   echo -ne '\n' | <yourfinecommandhere>
 }
